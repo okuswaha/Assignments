9073985ba33af5eb6e6ef9f47f9f2728a91897c8
prakash <kuswaha.java@gmail.com>
2015-10-29 21:16:28 -0400

    added model

--- Assignments/src/com/bfs/Operation.java
+++ Assignments/src/com/bfs/Operation.java
@@ -1,5 +1,20 @@
 package com.bfs;
 
 public class Operation {
+	private int a;
+	private int b;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	
 
 }
